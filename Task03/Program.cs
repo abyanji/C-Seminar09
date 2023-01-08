60975006849dfff83ea2efe42c3d2885c963a087
abyanji <a.byanjil99@gmail.com>
2023-01-09 02:05:00 +0300

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int DataEntry(string message)
+{
+    System.Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int SumOfAllNumbers(int num, int sum)
+{
+    if (num == 0) return sum;
+    sum += num;
+    return SumOfAllNumbers(num - 1, sum);
+}
+
+int number = DataEntry("Enter the number ");
+int sum = 0;
+sum = SumOfAllNumbers(number, sum);
+System.Console.WriteLine(sum);
