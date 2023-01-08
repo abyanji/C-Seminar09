489c9e36bebcac9264dc4ef6c5bdca734a0f40a0
abyanji <a.byanjil99@gmail.com>
2023-01-09 01:24:52 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int DataEntry(string message)
+{
+    System.Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int AllNumbersUpToSpec(int num, int count)
+{
+    if (count > num) return num;
+    System.Console.Write($"{count} ");
+    return AllNumbersUpToSpec(num, count + 1);
+}
+
+int number = DataEntry("Enter the number ");
+int count = 1;
+int allNumbers = AllNumbersUpToSpec(number, count);
+
