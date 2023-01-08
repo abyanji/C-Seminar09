c83b3d98e100e09b2c05c6a24ab84fb6e570c1e2
abyanji <a.byanjil99@gmail.com>
2023-01-09 01:34:15 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int DataEntry(string message)
+{
+    System.Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int AllNumbersFromSpecToSpec(int numOne, int numTwo)
+{
+    if (numTwo > numOne) return numOne;
+    System.Console.Write($"{numTwo} ");
+    return AllNumbersFromSpecToSpec(numOne, numTwo + 1);
+}
+
+int numberOne = DataEntry("Enter the number ");
+int numberTwo = DataEntry("Enter the number ");
+int allNumbers = AllNumbersFromSpecToSpec(numberOne, numberTwo);
